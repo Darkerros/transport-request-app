c28b44fa97c8ac5385605ed8e5e9e32441b44538
darker <darkerros@yandex.ru>
2023-11-09 07:05:35 +0300

    feat: create button with icon

---
+++ src/ui/button-with-icon/button-with-icon.tsx
@@ -0,0 +1,24 @@
+import { clsx } from "clsx";
+import type { ComponentProps, FC } from "react";
+
+import Button from "../button/button.tsx";
+
+import styles from './button-with-icon.module.scss'
+
+import Icons from "../icons/icons.tsx";
+
+interface ButtonWithIcon extends ComponentProps<typeof Button>, Pick<ComponentProps<typeof Icons>,'iconName'> {}
+
+const ButtonWithIcon: FC<ButtonWithIcon> = ({ extraClassName, children, iconName, ...otherProps }) => {
+
+  const buttonStyles = clsx({
+    [styles.buttonWithIcon]: true,
+    [`${extraClassName}`]: !!extraClassName
+  })
+
+  return (
+    <Button extraClassName={buttonStyles} {...otherProps}><Icons iconName={iconName}/>{ children }</Button>
+  );
+};
+
+export default ButtonWithIcon;
