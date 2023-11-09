4bb4669b0371a00aa78cd5f49a94d07018513e49
darker <darkerros@yandex.ru>
2023-11-09 07:36:52 +0300

    feat: add check icon and fix rerender icon

---
+++ src/ui/icons/check-icon.tsx
@@ -0,0 +1,9 @@
+const CheckIcon = () => {
+  return (
+    <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 6.5L5.5 11.5L11 1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
+
+export default CheckIcon;
--- src/ui/icons/icons.tsx
+++ src/ui/icons/icons.tsx
@@ -1,21 +1,26 @@
-import {FC} from "react";
-import PlusIcon from "./plus-icon.tsx";
+import {FC, memo} from "react";
 
-type IconName = 'plus';
+import PlusIcon from "./plus-icon.tsx";
+import CheckIcon from "./check-icon.tsx";
+
+type IconName = 'plus' | 'check';
 
 interface IconsProps {
   iconName: IconName;
 }
 
-const Icons: FC<IconsProps> = ({ iconName }) => {
+const Icons: FC<IconsProps> = memo(({ iconName }) => {
 
   switch (iconName) {
     case 'plus':
       return <PlusIcon/>
 
+    case 'check':
+      return <CheckIcon/>
+
     default:
       return null
   }
-};
+});
 
 export default Icons;
