e6c36a0adf643955f8477f678775e6e30cedb5c1
darker <darkerros@yandex.ru>
2023-11-10 12:15:27 +0300

    fix: icon attributes and button with icon rerender

--- src/ui/button-with-icon/button-with-icon.tsx
+++ src/ui/button-with-icon/button-with-icon.tsx
@@ -1,4 +1,5 @@
 import { clsx } from "clsx";
+import { memo } from "react";
 import type { ComponentProps, FC } from "react";
 
 import Button from "../button/button.tsx";
@@ -9,7 +10,7 @@ import Icons from "../icons/icons.tsx";
 
 interface ButtonWithIcon extends ComponentProps<typeof Button>, Pick<ComponentProps<typeof Icons>,'iconName'> {}
 
-const ButtonWithIcon: FC<ButtonWithIcon> = ({ extraClassName, children, iconName, ...otherProps }) => {
+const ButtonWithIcon: FC<ButtonWithIcon> = memo(({ extraClassName, children, iconName, ...otherProps }) => {
 
   const buttonStyles = clsx({
     [styles.buttonWithIcon]: true,
@@ -19,6 +20,6 @@ const ButtonWithIcon: FC<ButtonWithIcon> = ({ extraClassName, children, iconName
   return (
     <Button extraClassName={buttonStyles} {...otherProps}><Icons iconName={iconName}/>{ children }</Button>
   );
-};
+});
 
 export default ButtonWithIcon;
--- src/ui/icons/down-chevron.tsx
+++ src/ui/icons/down-chevron.tsx
@@ -1,7 +1,7 @@
 const DownChevron = () => {
   return (
     <svg width="54" height="32" viewBox="0 0 54 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M18.5 14L26.5 19.4172L35 14" stroke="#4C73E3" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"/>
+      <path d="M18.5 14L26.5 19.4172L35 14" stroke="#4C73E3" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"/>
     </svg>
   );
 };
