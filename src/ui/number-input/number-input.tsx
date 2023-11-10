c2b32395c548e23157122e7ad09c26a3d6250a8b
darker <darkerros@yandex.ru>
2023-11-10 12:14:01 +0300

    feat: add lable to number input

--- src/ui/number-input/number-input.tsx
+++ src/ui/number-input/number-input.tsx
@@ -2,19 +2,20 @@ import { DetailedHTMLProps, FC, InputHTMLAttributes, useEffect, useId, useState 
 
 import styles from './number-input.module.scss'
 
-interface NumberInputProps extends Pick<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,'name'> {
+interface NumberInputProps extends Pick<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,'name' | 'required'> {
   min?: number;
   max?: number;
   step?: number;
+  label?: string;
   value?: number;
   prefix?: string;
   onChange?: (value: number) => void;
 }
 
-const NumberInput: FC<NumberInputProps> = ({ onChange, value, prefix, step, min, max, name }) => {
+const NumberInput: FC<NumberInputProps> = ({ onChange, value, label, prefix, step, min, max, name, ...otherProps }) => {
   const inputId = useId()
 
-  const [localInputValue, setLocalInputValue] = useState<number>(value ?? 0)
+  const [localInputValue, setLocalInputValue] = useState<number>(value ? value : min ?? 0)
 
   useEffect(() => {
     /*
@@ -48,11 +49,12 @@ const NumberInput: FC<NumberInputProps> = ({ onChange, value, prefix, step, min,
 
   return (
     <div className={styles.numberInput}>
+      {label && <label htmlFor={inputId}>{label}</label>}
       <div className={styles.numberInput__inputContainer}>
-        <button onClick={handleAddingButtonClick} className={styles.numberInput__button}>+</button>
+        <button onClick={handleAddingButtonClick} type="button" className={styles.numberInput__button}>+</button>
         <span className={styles.numberInput__inputContent}>{localInputValue} {prefix}</span>
-        <button onClick={handleDeacreaseButtonClick} className={styles.numberInput__button}>-</button>
-        <input name={name} className={styles.numberInput__input} id={inputId} style={{ display: "none" }} readOnly value={localInputValue} type="number"/>
+        <button onClick={handleDeacreaseButtonClick} type="button" className={styles.numberInput__button}>-</button>
+        <input name={name} className={styles.numberInput__input} id={inputId} style={{ display: "none" }} {...otherProps} readOnly value={localInputValue} type="number"/>
       </div>
     </div>
 
